a36be03e3c4094a04db6694b961449346ac3e1fc
Marvin Meier <marvinofeld1991@web.de>
2019-07-21 21:48:25 +0200

    change text

--- src/components/details/Details.vue
+++ src/components/details/Details.vue
@@ -7,7 +7,7 @@
 
             <div class="details-info">
                 <p>
-                    Hi ich bin Kai und arbeite seit knapp 5 Jahren bei der OVB. OVB ist sehr cool und macht spaß. Wenn Sie Geld haben, geben Sie mir das bitte, danke.</p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
 
                 <div class="details-contact d-flex">
                     <ul class="details-contact-list">
